1341185cc4593fb47da4f8f330f12720be82c266
chaekie <chaekie.kim@gmail.com>
2021-07-16 15:22:40 +0900

    네비바 반응형 재적용

--- components/navbar/navbar.js
+++ components/navbar/navbar.js
@@ -1,7 +1,12 @@
-// 스크롤 내릴 때 네비바 사라짐, 스크롤 올릴 때 네비바 등장
 const body = document.querySelector("body");
 const navBar = body.querySelector("nav");
 let prevScrollPos = window.pageYOffset;
+const userAndDarkMode = navBar.querySelector(".userAndDarkMode ");
+const logout = userAndDarkMode.querySelector(".logout");
+const darkMode = userAndDarkMode.querySelector(".dark-mode");
+const lightBtn = darkMode.querySelector(".light");
+const darkBtn = darkMode.querySelector(".dark");
+const ellipsisBtn = navBar.querySelector(".ellipsis");
 
 window.addEventListener("scroll", () => {
   let currentScrollPos = window.pageYOffset;
@@ -11,4 +16,18 @@ window.addEventListener("scroll", () => {
     navBar.style.top = "-70px";
   }
   prevScrollPos = currentScrollPos;
+});
+
+lightBtn.addEventListener("click", () => {
+  lightBtn.classList.toggle("deactive");
+  darkBtn.classList.toggle("active");
+});
+
+darkBtn.addEventListener("click", () => {
+  darkBtn.classList.toggle("active");
+  lightBtn.classList.toggle("deactive");
+});
+
+ellipsisBtn.addEventListener("click", () => {
+  userAndDarkMode.classList.toggle("active");
 });
