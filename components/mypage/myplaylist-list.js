ab5e3cf02a086d74f476bdfe6550e3dd36b94409
gajeong <rlarkwjd127@gmail.com>
2021-07-18 21:25:48 +0900

    내 플레이리스트 좋아요 추가

--- components/mypage/myplaylist-list.js
+++ components/mypage/myplaylist-list.js
@@ -1,5 +1,17 @@
+const unlike = document.querySelector(".unlike");
+const like = document.querySelector(".like");
 function onButton(obj) {
   const parent = obj.parentNode;
   console.log(parent)
   parent.parentNode.removeChild(parent)
 }
+
+unlike.addEventListener("click", () => {
+  unlike.classList.remove("active");
+  like.classList.add("active")
+})
+like.addEventListener("click", () => {
+  like.classList.remove("active");
+  unlike.classList.add("active");
+})
+
