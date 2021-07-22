ab7dba708848eaf9cf1b9b4559720446f84f37a2
gajeong <rlarkwjd127@gmail.com>
2021-07-23 02:00:26 +0900

    fetch 해서 플레이리스트 담기

--- static/components/mypage/myplaylist-list.js
+++ static/components/mypage/myplaylist-list.js
@@ -1,21 +1,33 @@
 //유저 음악정보 가져오기,, 
-function displayData() {
-
+function displayItems(items) {
+  items[0].tracks.forEach((music) => {
+    let str = `<div class="item">
+    <div class="cover"><img src="${music.track_image}" alt="coverImg"></div>
+    <div class="title">${music.track_name}</div>
+    <div class="artist">${music.track_artist}</div>
+    <div class="album">${music.track_album}</div>
+    <div class="remove material-icons-outlined" onclick="onButton(this)">delete_outline</div>
+    </div>`;
+    $(".items").append(str);
+  });
 }
 function loadData() {
-  return fetch("https://nochu.pw/playlist_api/?uid=user&title=title")
+  //회원 정보에 따라 user이랑 title 변경 해야함 
+  return fetch("https://nochu.pw/playlist_api/playlist/?uid=user&title=title")
     .then(res => {
       console.log(res);
       return res.json();
     })
-    .then(data => {
-      console.log(data)
-    })
-    .catch(error => {
-      console.log("error", error)
-    });
 }
 
+// const data = loadData();
+// // displayData(data);
+
+loadData()
+  .then(items => {
+    console.log(items)
+    displayItems(items);
+  })
 
 
 const unlike = document.querySelector(".unlike");
